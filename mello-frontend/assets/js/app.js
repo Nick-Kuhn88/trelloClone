067e777ed992eeafe29f0cd1425623c4ab433d95
Nick Kuhn <nick.kuhn88@gmail.com>
2019-11-21 03:14:49 +0000

    halfway through lesson 13

--- mello-frontend/assets/js/app.js
+++ mello-frontend/assets/js/app.js
@@ -13,9 +13,37 @@ function handleListCreate() {
 
 function handleCardCreate(event) {
   var $listContainer = event.target.parentNode;
-  var listId = $listContainer.getAttribute('data-id');
+  var listId = Number($listContainer.getAttribute('data-id'));
 
-  alert(`clicked list ${listId}`);
+  var cardText = prompt('New card text') || '';
+
+  if (cardText.trim()) {
+    board.addCard(listId, cardText);
+    renderBoard();
+  }
+}
+
+function handleListEdit(event) {
+  var $listContainer = event.target.parentNode.parentNode;
+  var listId = Number($listContainer.getAttribute('data-id'));
+
+  var listTitle = prompt('New list title') || '';
+
+  if (listTitle.trim()) {
+    board.editList(listId, listTitle);
+    renderBoard();
+  }
+}
+
+function handleCardEdit(event) {
+  var cardId = Number(event.target.getAttribute('data-id'));
+
+  var cardText = prompt('New card text') || '';
+
+  if (cardText.trim()) {
+    board.editCard(cardId, cardText);
+    renderBoard();
+  }
 }
 
 function renderBoard() {
@@ -30,6 +58,21 @@ function renderBoard() {
 
     var $headerButton = document.createElement('button');
     $headerButton.textContent = list.title;
+    $headerButton.addEventListener('click', handleListEdit);
+
+    var $cardUl = document.createElement('ul');
+
+    list.cards.forEach(function(card) {
+      var $cardLi = document.createElement('li');
+
+      var $cardButton = document.createElement('button');
+      $cardButton.textContent = card.text;
+      $cardButton.setAttribute('data-id', card.id);
+      $cardButton.addEventListener('click', handleCardEdit);
+
+      $cardLi.appendChild($cardButton);
+      $cardUl.appendChild($cardLi);
+    });
 
     var $addCardButton = document.createElement('button');
     $addCardButton.textContent = 'Add a card...';
@@ -37,6 +80,7 @@ function renderBoard() {
 
     $header.appendChild($headerButton);
     $listContainer.appendChild($header);
+    $listContainer.appendChild($cardUl);
     $listContainer.appendChild($addCardButton);
     $boardContainer.appendChild($listContainer);
   });
--- mello-frontend/assets/js/board.js
+++ mello-frontend/assets/js/board.js
@@ -18,6 +18,14 @@ class List {
     var card = new Card(text);
     this.cards.push(card);
   }
+
+  findCard(cardId) {
+    return this.cards.find(function(card) {
+      if (cardId === card.id) {
+        return card;
+      }
+    });
+  }
 }
 
 List.lastId = 0;
@@ -31,6 +39,35 @@ class Board {
   addList(text) {
     var list = new List(text);
     this.lists.push(list);
+  }
+
+  findList(listId) {
+    return this.lists.find(function(list) {
+      return listId === list.id;
+    });
+  }
+
+  editList(listId, newTitle) {
+    var list = this.findList(listId);
+    if (list) {
+      list.title = newTitle;
+    }
+  }
+
+  addCard(listId, cardText) {
+    var list = this.findList(listId);
+    if (list) {
+      list.addCard(cardText);
+    }
+  }
+
+  editCard(cardId, cardText) {
+    this.lists.forEach(function(list) {
+      var card = list.findCard(cardId);
+      if (card) {
+        card.text = cardText;
+      }
+    });
   }
 }
 
